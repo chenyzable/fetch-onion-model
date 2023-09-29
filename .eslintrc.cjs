b2c7f93b6fab7a844c17a286733def239e5722af
chenyuzhong <chenyuzhong@sto.cn>
2023-09-30 01:09:05 +0800

    chore: 集成release-it

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,7 @@ module.exports = {
   extends: [require.resolve("@cyzeal/lint-config-lib/eslint-ts")],
   rules: {
     "@typescript-eslint/no-non-null-assertion": 0,
+    "@typescript-eslint/no-explicit-any": 0,
     "no-underscore-dangle": 0,
   },
 }
